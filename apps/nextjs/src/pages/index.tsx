e7ae79554ddca0d2a2749e5124114e98c1fc46e2
VincentAudette <36488653+VincentAudette@users.noreply.github.com>
2023-05-11 15:00:05 -0400

    Update index.tsx

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import TopNav from "../components/TopNav";
-import SideBarLayout from "../components/SideBarLayout";
-import ProjectCard from "../components/ProjectCard";
 import { useAuth } from "@clerk/nextjs";
 import WelcomeSection from "../components/WelcomeSection";
 import { usePFEAuth } from "../context/PFEAuthContext";
-import { Post } from "@acme/db";
 import StudentView from "../components/RoleViews/StudentView";
 import PromoterView from "../components/RoleViews/PromoterView";
 import AdminView from "../components/RoleViews/AdminView";
@@ -15,8 +12,6 @@ import { useEffect } from "react";
 import DeveloperView from "../components/RoleViews/DeveloperView";
 
 export default function Home() {
-  // const postQuery = trpc.post.all.useQuery();
-
   const { isSignedIn, userId: clerkId } = useAuth();
   const { data: getUserData } = trpc.auth.getUser.useQuery(clerkId as string, {
     enabled: !!isSignedIn,
@@ -45,11 +40,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-neutral-50 dark:bg-neutral-600">
-        <TopNav
-          isSignedIn={isSignedIn}
-          userData={userData}
-          activeRole={activeRole}
-        />
+        <TopNav isSignedIn={isSignedIn} activeRole={activeRole} />
         <div className=" flex w-full max-w-5xl justify-between gap-10 px-4 py-10 sm:px-12 xl:max-w-[80rem] 2xl:max-w-[100rem]">
           {activeRole === "STUDENT" && <StudentView />}
           {activeRole === "PROMOTER" && <PromoterView />}
@@ -62,13 +53,3 @@ export default function Home() {
     </>
   );
 }
-
-// {postQuery.data ? (
-//   <div className="flex w-full flex-col gap-4">
-//       {postQuery.data?.map((p: Post) => {
-//       return <ProjectCard key={p.id} post={p} />;
-//       })}
-//   </div>
-//   ) : (
-//   <p>Loading..</p>
-//   )}
