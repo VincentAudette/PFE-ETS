67a77440e2ba07cf9d0d920ab8cfc3ea5611fb7a
VincentAudette <36488653+VincentAudette@users.noreply.github.com>
2023-05-17 18:13:36 -0400

    Add registration path

--- apps/nextjs/src/components/RoleViews/PromoterView.tsx
+++ apps/nextjs/src/components/RoleViews/PromoterView.tsx
@@ -14,6 +14,7 @@ import {
 import { PlusCircleIcon, PlusIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { NextRouter, Router, useRouter } from "next/router";
+import { usePFEAuth } from "../../context/PFEAuthContext";
 
 const navigation: NavigationItem[] = [
   {
@@ -46,6 +47,10 @@ export default function PromoterView({
 
   console.log("router.asPath", router.asPath);
 
+  const { userData } = usePFEAuth();
+
+  console.log("userData", userData);
+
   if (
     navigation[0] !== undefined &&
     navigation[1] !== undefined &&
@@ -66,6 +71,10 @@ export default function PromoterView({
       navigation[3].current = false;
     }
   }
+
+  // if (userData.role === "UNASSIGNED") {
+  //   return <div>TEST</div>;
+  // }
 
   return (
     <SideBarLayout
---
+++ apps/nextjs/src/pages/registration.tsx
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+import TopNav from "../components/TopNav";
+import { useAuth } from "@clerk/nextjs";
+import { usePFEAuth } from "../context/PFEAuthContext";
+import StudentView from "../components/RoleViews/StudentView";
+import PromoterView from "../components/RoleViews/PromoterView";
+import AdminView from "../components/RoleViews/AdminView";
+import { useEffect } from "react";
+import DeveloperView from "../components/RoleViews/DeveloperView";
+import Link from "next/link";
+
+export default function Home() {
+  const { isSignedIn, userId: clerkId } = useAuth();
+  const { data: getUserData } = trpc.auth.getUser.useQuery(clerkId as string, {
+    enabled: !!isSignedIn,
+  });
+
+  const { userData, setUserData, authProfile } = usePFEAuth();
+
+  useEffect(() => {
+    if (getUserData !== undefined && authProfile === null) {
+      setUserData(getUserData);
+    }
+  });
+
+  const activeRole = authProfile !== null ? authProfile : userData?.role;
+
+  return (
+    <>
+      <Head>
+        <title>
+          Projet de fin d&apos;études - École de Technologie Supérieur
+        </title>
+        <meta
+          name="description"
+          content="Application pour supporter les projets de fin d'études."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center bg-neutral-50">
+        <TopNav isSignedIn={isSignedIn} activeRole={activeRole} />
+        <div className="h-10" />
+        {activeRole === "STUDENT" && <StudentView />}
+        {activeRole === "PROMOTER" && <PromoterView />}
+        {activeRole === "ADMIN" && <AdminView />}
+        {activeRole === "DEVELOPER" && <DeveloperView />}
+        {userData === null && (
+          <div>
+            NOT AUTHORIZED
+            <Link href="/login"></Link>
+          </div>
+        )}
+      </main>
+    </>
+  );
+}
