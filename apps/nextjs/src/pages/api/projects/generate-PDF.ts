70655db4229d9e638c25f05ae4866f7584009cef
alexistamand <alex.p.n.b@hotmail.com>
2023-06-20 16:44:41 -0400

    Added Azure blob upload

--- apps/nextjs/src/pages/api/projects/generate-PDF.ts
+++ apps/nextjs/src/pages/api/projects/generate-PDF.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { generate } from "@pdfme/generator";
 import { prisma } from "../../../../../../packages/db/index";
 import { template, generateInputs } from "./generate-PDF-Helper";
+import { BlobServiceClient } from "@azure/storage-blob";
 import fs from "fs";
 import * as path from "path";
 
@@ -17,6 +18,26 @@ export default async function handler(
     const inputs = await generateInputs(project);
 
     const pdf = await generate({ template, inputs });
+
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.AZURE_CONNECTION_STRING ?? "",
+    );
+
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient("pdf-blob");
+
+    // Create a unique name for the blob
+    const blobName = `TestUpload${Date.now()}`;
+
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    // Upload data to the blob
+    const uploadBlobResponse = await blockBlobClient.upload(
+      pdf,
+      pdf.byteLength,
+    );
 
     // Node.js
     const pdfFileName = "test4.pdf";
