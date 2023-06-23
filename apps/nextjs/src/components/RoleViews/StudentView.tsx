f689be4b8ce5f562af0576b350bc1fa24d5a89ec
VincentAudette <36488653+VincentAudette@users.noreply.github.com>
2023-06-23 16:33:01 -0400

    Update StudentView.tsx

--- apps/nextjs/src/components/RoleViews/StudentView.tsx
+++ apps/nextjs/src/components/RoleViews/StudentView.tsx
@@ -6,6 +6,7 @@ import InputWithIcon from "../Forms/atoms/InputWithIcon";
 import SimpleSelect from "../Forms/atoms/SimpleSelect";
 import ComboBox, { Option } from "../Forms/atoms/ComboBox";
 import { useState } from "react";
+import Modal from "../Forms/atoms/Modal";
 
 const options: Option[] = [
   {
@@ -61,6 +62,7 @@ export default function StudentView() {
   const [selectedPfe4, setSelectedPfe4] = useState<Option | undefined>(
     options[0],
   );
+  const [pdfView, setPdfViewed] = useState<string | null>(null);
 
   const handleSelectionSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -79,69 +81,78 @@ export default function StudentView() {
     console.log(formValues);
   };
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Formulaire de choix de PFE</h1>
-      <div className="h-3" />
-      <InfoAlert text="Si votre PFE vous a déjà été attribué, alors choisissez le meme PFE pour vos 3 premiers choix et ajouter un autre PFE pour votre 4eme choix. Si votre groupe contient au moins 3 personnes, alors choisissez le meme PFE pour vos 4 choix." />
-      <div className="h-3" />
-      <form className=" flex flex-col gap-4" onSubmit={handleSelectionSubmit}>
-        {[
-          {
-            selectedPfe: selectedPfe1,
-            setSelectedPfe: setSelectedPfe1,
-          },
-          {
-            selectedPfe: selectedPfe2,
-            setSelectedPfe: setSelectedPfe2,
-          },
-          {
-            selectedPfe: selectedPfe3,
-            setSelectedPfe: setSelectedPfe3,
-          },
-          {
-            selectedPfe: selectedPfe4,
-            setSelectedPfe: setSelectedPfe4,
-          },
-        ].map((pfe, i) => (
-          <ComboBox
-            key={"pfe_choice_" + (i + 1)}
-            name={"pfeChoice" + (i + 1)}
-            options={options}
-            label={`${i + 1}. Quel est votre ${
-              ["PREMIER", "DEUXIÈME", "TROISIÈME", "QUATRIÈME"][i]
-            } choix de projet?`}
-            selectedOption={pfe.selectedPfe}
-            setSelectedOption={pfe.setSelectedPfe}
-          />
-        ))}
-        <InputWithIcon
-          type="text"
-          name="groupePreference"
-          id="groupePreference"
-          label="Préférence de groupe - Courriels délimité par des virgules"
-          Icon={UserPlusIcon}
-          placeholder="jane.doe@ens.etsmtl.ca,john.doe@ens.etsmtl.ca (Maximum 4 personnes)"
-        />
-        <CheckBoxInput
-          id="isPreApprovedInPfe"
-          name="isPreApprovedInPfe"
-          label="Je confirme être éligible à sélectionné multiple fois le même PFE, car j’ai été préalablement accepté pour cette option."
-        />{" "}
-        <CheckBoxInput
-          id="isHealthProfile"
-          name="isHealthProfile"
-          label="Je confirme avoir sélectionné des PFE liés à la Santé, conformément à mon profil Santé."
-        />
-        <CheckBoxInput
-          id="isEnerngyProfile"
-          name="isEnerngyProfile"
-          label="Je confirme avoir sélectionné des PFE en lien avec l’Institut en génie de l’énergie électrique (IGEE), conformément à mon profil IGEE."
-        />
+    <>
+      <Modal
+        title="Aperçu du PDF"
+        open={pdfView !== null}
+        setOpen={() => setPdfViewed(null)}
+      >
+        {pdfView && <iframe src={pdfView} className="h-full w-full" />}
+      </Modal>
+      <div>
+        <h1 className="text-2xl font-bold">Formulaire de choix de PFE</h1>
         <div className="h-3" />
-        <div className=" self-center">
-          <Button text="Confirmer mes choix" type="submit" />
-        </div>
-      </form>
-    </div>
+        <InfoAlert text="Si votre PFE vous a déjà été attribué, alors choisissez le meme PFE pour vos 3 premiers choix et ajouter un autre PFE pour votre 4eme choix. Si votre groupe contient au moins 3 personnes, alors choisissez le meme PFE pour vos 4 choix." />
+        <div className="h-3" />
+        <form className=" flex flex-col gap-4" onSubmit={handleSelectionSubmit}>
+          {[
+            {
+              selectedPfe: selectedPfe1,
+              setSelectedPfe: setSelectedPfe1,
+            },
+            {
+              selectedPfe: selectedPfe2,
+              setSelectedPfe: setSelectedPfe2,
+            },
+            {
+              selectedPfe: selectedPfe3,
+              setSelectedPfe: setSelectedPfe3,
+            },
+            {
+              selectedPfe: selectedPfe4,
+              setSelectedPfe: setSelectedPfe4,
+            },
+          ].map((pfe, i) => (
+            <ComboBox
+              key={"pfe_choice_" + (i + 1)}
+              name={"pfeChoice" + (i + 1)}
+              options={options}
+              label={`${i + 1}. Quel est votre ${
+                ["PREMIER", "DEUXIÈME", "TROISIÈME", "QUATRIÈME"][i]
+              } choix de projet?`}
+              selectedOption={pfe.selectedPfe}
+              setSelectedOption={pfe.setSelectedPfe}
+            />
+          ))}
+          <InputWithIcon
+            type="text"
+            name="groupePreference"
+            id="groupePreference"
+            label="Préférence de groupe - Courriels délimité par des virgules"
+            Icon={UserPlusIcon}
+            placeholder="jane.doe@ens.etsmtl.ca,john.doe@ens.etsmtl.ca (Maximum 4 personnes)"
+          />
+          <CheckBoxInput
+            id="isPreApprovedInPfe"
+            name="isPreApprovedInPfe"
+            label="Je confirme être éligible à sélectionné multiple fois le même PFE, car j’ai été préalablement accepté pour cette option."
+          />{" "}
+          <CheckBoxInput
+            id="isHealthProfile"
+            name="isHealthProfile"
+            label="Je confirme avoir sélectionné des PFE liés à la Santé, conformément à mon profil Santé."
+          />
+          <CheckBoxInput
+            id="isEnerngyProfile"
+            name="isEnerngyProfile"
+            label="Je confirme avoir sélectionné des PFE en lien avec l’Institut en génie de l’énergie électrique (IGEE), conformément à mon profil IGEE."
+          />
+          <div className="h-3" />
+          <div className=" self-center">
+            <Button text="Confirmer mes choix" type="submit" />
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
