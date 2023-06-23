dfa1b12f3f04548389266bba03ce721835b9efd0
VincentAudette <36488653+VincentAudette@users.noreply.github.com>
2023-06-23 14:59:42 -0400

    Hopefully this works
    https://docs.svix.com/receiving/verifying-payloads/how#nodejs-nextjs

--- apps/nextjs/src/pages/api/clerk-webhook.ts
+++ apps/nextjs/src/pages/api/clerk-webhook.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Role, prisma } from "../../../../../packages/db/index";
 import { Webhook } from "svix";
+import { buffer } from "micro";
 
 export const config = {
   api: {
@@ -14,41 +15,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const payload = (await buffer(req)).toString();
+  const headers = req.headers;
   const wh = new Webhook(secret);
-  let msg;
+
+  let msg: any;
   try {
-    msg = wh.verify(JSON.stringify(req.body), {
-      "svix-id": (req.headers["svix-id"] as string) || "",
-      "svix-timestamp": (req.headers["svix-timestamp"] as string) || "",
-      "svix-signature": (req.headers["svix-signature"] as string) || "",
-    });
-    const data = req?.body?.data;
-
-    if (!data) {
-      res.status(400).json({ error: "No data in body", body: req.body, msg });
-      return;
-    }
-
-    await prisma.user
-      .create({
-        data: {
-          clerkId: data.id,
-          firstName: data.first_name,
-          lastName: data.last_name,
-          email: data.email_addresses[0].email_address,
-          role: Role.UNREGISTERED,
-        },
-      })
-      .then(() => {
-        console.log("User added");
-        res.status(200).json({ status: "User added" }); // Added json response
-      })
-      .catch((error: any) => {
-        console.error(error);
-        res.status(500).json({ error: "There was an error" }); // Added json response
-      });
+    msg = wh.verify(payload, headers as any);
   } catch (err) {
     console.log("Error: ", err);
     res.status(400).json({ error: "Invalid webhook signature", msg });
   }
+
+  console.log("Message: ", msg);
+
+  const data = msg?.data;
+
+  if (!data) {
+    res.status(400).json({ error: "No data in body", body: req.body, msg });
+    return;
+  }
+
+  await prisma.user
+    .create({
+      data: {
+        clerkId: data.id,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email_addresses[0].email_address,
+        role: Role.UNREGISTERED,
+      },
+    })
+    .then(() => {
+      console.log("User added");
+      res.status(200).json({ status: "User added" }); // Added json response
+    })
+    .catch((error: any) => {
+      console.error(error);
+      res.status(500).json({ error: "There was an error" }); // Added json response
+    });
 }
