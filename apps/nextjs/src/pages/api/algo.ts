98124c1c70f4e7723c4741d5db4b249b43383627
Vincent <vincent.fournier.4@ens.etsmtl.ca>
2023-10-28 18:00:46 -0400

    Objects created

--- apps/nextjs/src/pages/api/algo.ts
+++ apps/nextjs/src/pages/api/algo.ts
@@ -1,30 +1,80 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as fs from "fs";
-import csv from 'csv-parser';
+import csv from "csv-parser";
 
-async function readCSVFile(filePath: string): Promise<object[]> {
-  const results: object[] = [];
+interface Etudiant {
+  Courriel: string;
+  Choix1: string;
+  Choix2: string;
+  Choix3: string;
+  Choix4: string;
+}
 
+interface Projet {
+  Nom: string;
+  MinEtudiants: number;
+  MaxEtudiants: number;
+}
+
+async function readStudents(filePath: string): Promise<Etudiant[]> {
   return new Promise((resolve, reject) => {
+    const resultsStudents: Etudiant[] = [];
+
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', () => resolve(results))
-      .on('error', (error) => reject(error));
+      .on("data", (data) => {
+        const etudiant: Etudiant = {
+          Courriel: data.Courriel,
+          Choix1: data.Choix1,
+          Choix2: data.Choix2,
+          Choix3: data.Choix3,
+          Choix4: data.Choix4,
+        };
+        resultsStudents.push(etudiant);
+      })
+      .on("end", () => resolve(resultsStudents))
+      .on("error", (error) => reject(error));
   });
 }
 
-const filePath = 'public/etudiants.csv';
+async function readProjects(filePath: string): Promise<Projet[]> {
+  return new Promise((resolve, reject) => {
+    const resultsProjects: Projet[] = [];
+
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        const projet: Projet = {
+          Nom: data.Nom,
+          MinEtudiants: parseInt(data.Min, 10),
+          MaxEtudiants: parseInt(data.Max, 10),
+        };
+        resultsProjects.push(projet);
+      })
+      .on("end", () => resolve(resultsProjects))
+      .on("error", (error) => reject(error));
+  });
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Chemins des différents fichiers
+  const filePathStudents = "public/etudiants.csv";
+  const filePathProjects = "public/projets.csv";
+
   try {
-    // Code de l'algorithme
-    const data = await readCSVFile(filePath);
-    data.forEach( (etudiant:any) => {
-      console.log(etudiant.Choix1)
+    const ListeEtudiants = await readStudents(filePathStudents);
+    const ListeProjets = await readProjects(filePathProjects);
+
+    // Algorithme
+    ListeEtudiants.forEach((etudiant) => {
+      console.log(etudiant);
     });
-    // Renvoi du JSON formatté
-    res.status(200).json(data);
+
+    ListeProjets.forEach((projet) => {
+      console.log(projet);
+    });
+
+    res.status(200).json({ etudiants: ListeEtudiants, projets: ListeProjets });
   } catch (error) {
     console.error(error);
     res.status(400).json(error);
